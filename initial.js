6986d320262c2adff534f471c29747fb66e4951d
Raahl Prem <raahulprem16@gmail.com>
2020-08-03 15:53:32 +0530

    first commit

---
+++ initial.js
@@ -0,0 +1,9 @@
+document.getElementById('login').addEventListener("click", 
+function(){document.querySelector('.login-model').style.display = "flex"; });
+
+document.querySelector('.close').addEventListener("click", 
+function(){ document.querySelector('.login-model').style.display = 'none';});
+
+
+
+// alert("The file is working");
